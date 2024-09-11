a460dcc74f3c9e567f530b5e96d6c1d099c25422
Gamin8ing <anim8tor.ing@gmail.com>
2024-09-11 17:22:23 +0530

    major ui change

--- index.js
+++ index.js
@@ -2,22 +2,48 @@ var categories = document.getElementsByClassName("category");
 
 for (var i = 0; i < categories.length; i++) {
 	categories[i].addEventListener("click", function () {
-		const sleepDiv = this.children[1].children[0];
+		const standDiv = this.children[1].children[0];
 		const cat = this.parentElement;
 		const ques = this.nextElementSibling;
-		if (sleepDiv.classList.contains("anim")) {
-			sleepDiv.classList.remove("anim");
-			sleepDiv.classList.add("anim-reverse");
+		if (standDiv.classList.contains("anim")) {
+			console.log("if");
+			standDiv.classList.remove("anim");
+			standDiv.classList.add("anim-reverse");
 			ques.style.gridTemplateRows = "0fr";
 			ques.style.padding = "0";
+			// for (let j = 0; j < categories.length; j++) {
+			// 	if (j === i) continue;
+			// 	const otherstandDiv = categories[j].children[1].children[0];
+			// 	const otherQues = categories[j].nextElementSibling;
+			// 	otherstandDiv.classList.add("anim");
+			// 	otherstandDiv.classList.remove("anim-reverse");
+			// 	otherQues.style.gridTemplateRows = "1fr";
+			// 	otherQues.style.padding = "20px 40px";
+			// }
+
 			// cat.style.maxHeight = this.scrollHeight + "px";
 		} else {
-			sleepDiv.classList.remove("anim-reverse");
-			sleepDiv.classList.add("anim");
+			console.log("else");
+			standDiv.classList.add("anim");
+			if (standDiv.classList.contains("anim-reverse")) {
+				standDiv.classList.remove("anim-reverse");
+			}
+			for (let j = 0; j < categories.length; j++) {
+				if (categories[j] === this) continue;
+				const otherstandDiv = categories[j].children[1].children[0];
+				const otherQues = categories[j].nextElementSibling;
+				if (otherstandDiv.classList.contains("anim")) {
+					otherstandDiv.classList.remove("anim");
+					otherstandDiv.classList.add("anim-reverse");
+					otherQues.style.gridTemplateRows = "0fr";
+					otherQues.style.padding = "0";
+					break;
+				}
+			}
 			ques.style.gridTemplateRows = "1fr";
 			ques.style.padding = "20px 40px";
 			// cat.style.maxHeight = "fit-content";
 		}
-		// console.log(sleepDiv);
+		// console.log(standDiv);
 	});
 }
